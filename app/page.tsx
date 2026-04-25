4ac25b26fbe79335a00089bb91f82ea1e833b5ee
vorod13 <markthemark13@gmail.com>
2026-04-24 18:30:07 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <iframe
-      src="/index.html"
-      style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        border: 'none',
-      }}
-      title="Kantan Landing Page"
-    />
-  );
+  redirect('/index.html');
 }
