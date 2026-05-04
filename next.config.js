aa264dc1ec625c240960d456beefa6c02465a2f7
vorod13 <markthemark13@gmail.com>
2026-05-04 15:33:48 -0700

    removing legacy index replacing with react

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/index.html',
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
